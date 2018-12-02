e954ebe7bac395030fd864da920a38e443479667
qinghon <wushengshijie@outlook.com>
2018-12-02 13:03:27 +0800

    测试新判断

--- bonuscloud/bonuscloudinstall.sh
+++ bonuscloud/bonuscloudinstall.sh
@@ -1,95 +1,105 @@
 #!/bin/sh
 
-ARM="https://raw.githubusercontent.com/hikaruchang/BonusCloud-Node/master/openwrt-ipk/bonuscloud_0.2.2-6o-1_arm_cortex-a9.ipk"
-MIPS="https://github.com/hikaruchang/BonusCloud-Node/raw/master/openwrt-ipk/bonuscloud_0.2.2-6o-1_mips_24kc.ipk"
-MIPSEL="https://github.com/hikaruchang/BonusCloud-Node/raw/master/openwrt-ipk/bonuscloud_0.2.2-6o-1_mipsel_24kc.ipk"
+ARM="https://github.com/hikaruchang/BonusCloud-Node/raw/master/openwrt-ipk/bonuscloud_0.2.2-7o-1_arm_cortex-a9.ipk"
+MIPS="https://github.com/hikaruchang/BonusCloud-Node/raw/master/openwrt-ipk/bonuscloud_0.2.2-7o-1_mips_24kc.ipk"
+MIPSEL="https://github.com/hikaruchang/BonusCloud-Node/raw/master/openwrt-ipk/bonuscloud_0.2.2-7o-1_mipsel_24kc.ipk"
 
 opkg_init(){
-	opkg update
-	opkg install wget curl luci-lib-jsonc  liblzo libcurl libopenssl libstdcpp libltdl ca-certificates ca-bundle ip6tables kmod-ip6tables kmod-ip6tables-extra kmod-nf-ipt6 ip6tables-mod-nat ip6tables-extra ip6tables-mod-nat kmod-tun
+    opkg update
+    opkg install wget curl luci-lib-jsonc  liblzo libcurl libopenssl libstdcpp libltdl ca-certificates ca-bundle ip6tables kmod-ip6tables kmod-ip6tables-extra kmod-nf-ipt6 ip6tables-mod-nat ip6tables-extra ip6tables-mod-nat kmod-tun
 }
 
 arm_ins(){
-	opkg_init
-	rm /tmp/bonuscloud*
-	cd /tmp&&curl -L -k $ARM -o bonuscloudarm.ipk
-	opkg install /tmp/bonuscloudarm.ipk
-	res=`echo $?`
-	echo "$res"
-	if [ "$res" == 0 ]; then 
-		echo -e "\033[32m Install Success!\033[0m"
-	else
-		echo -e "\033[31m Install Failed!\033[0m"
-	fi
+    opkg_init
+    rm /tmp/bonuscloud*
+    cd /tmp&&curl -L -k $ARM -o bonuscloudarm.ipk
+    opkg install /tmp/bonuscloudarm.ipk
+    res=`echo $?`
+    echo "$res"
+    if [ "$res" == 0 ]; then 
+        echo -e "\033[32m Install Success!\033[0m"
+    else
+        echo -e "\033[31m Install Failed!\033[0m"
+    fi
 }
 mips_ins(){
-	opkg_init
-	rm /tmp/bonuscloud*
-	cd /tmp&&curl -L -k $MIPS -o bonuscloudmips.ipk
-	opkg install /tmp/bonuscloudmips.ipk
-	res=`echo $?`
-	echo "$res"
-	if [ "$res" == 0 ]; then 
-		echo -e "\033[32m Install Success!\033[0m"
-	else
-		echo -e "\033[31m Install Failed!\033[0m"
-	fi
+    opkg_init
+    rm /tmp/bonuscloud*
+    cd /tmp&&curl -L -k $MIPS -o bonuscloudmips.ipk
+    opkg install /tmp/bonuscloudmips.ipk
+    res=`echo $?`
+    echo "$res"
+    if [ "$res" == 0 ]; then 
+        echo -e "\033[32m Install Success!\033[0m"
+    else
+        echo -e "\033[31m Install Failed!\033[0m"
+    fi
 }
 mipsel_ins(){
-	opkg_init
-	rm /tmp/bonuscloud*
-	cd /tmp&&curl -L -k $MIPSEL -o bonuscloudmipsel.ipk
-	opkg install /tmp/bonuscloudmipsel.ipk
-	res=`echo $?`
-	echo "$res"
-	if [ "$res" == 0 ]; then 
-		echo -e "\033[32m Install Success!\033[0m"
-	else
-		echo -e "\033[31m Install Failed!\033[0m"
-	fi
+    opkg_init
+    rm /tmp/bonuscloud*
+    cd /tmp&&curl -L -k $MIPSEL -o bonuscloudmipsel.ipk
+    opkg install /tmp/bonuscloudmipsel.ipk
+    res=`echo $?`
+    echo "$res"
+    if [ "$res" == 0 ]; then 
+        echo -e "\033[32m Install Success!\033[0m"
+    else
+        echo -e "\033[31m Install Failed!\033[0m"
+    fi
 }
-
+language_ins(){
+    
+    read -p "Install language package? like fr , if n exit. default:[zh-cn]: " LANG
+    if [ -z $LANG ]; then
+        LANG="zh-cn"
+    elif [ "$LANG" == "n" -o "$LANG" == "N" ]; then
+        exit 0
+    fi
+    echo "language paskage :  luci-i18n-base-"$LANG""
+    opkg install  luci-i18n-base-"$LANG"
+    exit 0
+}
 defult_ins(){
-	echo -e " \033[31m 注意少部分路由器如K2P,新路由3会判断失败，请手动加上参数执行\033[0m "
-	echo "比如\n bonuscloudinstall.sh mipsel \n"
-	echo "装错了不要紧，卸载后重新来就好"
-	cpu=`uname -m`
-	if [[ -n ""$cpu"|grep arm" ]]; then
-		#statements
-		echo -e " the cpu is\033[31m $cpu\033[0m ,install arm"
-		arm_ins
-	elif [[ -n ""$cpu"|grep mips" ]]; then
-		
-		judge_k2p=`uname -n |grep K2P`
-		if [[ -n "$judge_k2p" ]]; then
-			
-			echo "device is K2P ,install mipsel"
-			echo "K2P can not install for now,because have more problem ,but I try it"
-	        mipsel_ins
-
-		else
-			echo -e " the cpu is\033[31m $cpu\033[0m ,install mips"
-			mips_ins
-		fi
-	elif [[ -n ""$cpu"|grep mipsel" ]]; then
-		mipsel_ins
-	else
-		echo "you device can not install the package"
-	fi
+    echo -e " \033[31m 注意少部分路由器如K2P,新路由3会判断失败，请手动加上参数执行\033[0m "
+    echo "比如\n bonuscloudinstall.sh mipsel \n"
+    echo "装错了不要紧，卸载后重新来就好"
+    cpu=`cat /etc/opkg/distfeeds.conf`
+    if [ `echo $?` -eq 0 ]; then
+        if [ -n ""$cpu"|grep arm_" ]; then
+            echo -e " the cpu is\033[31m $cpu\033[0m ,install arm"
+            arm_ins
+        elif [ -n ""$cpu"|grep mips_" ]; then
+            echo -e " the cpu is\033[31m $cpu\033[0m ,install mips"
+            mips_ins
+        elif [ -n ""$cpu"|grep mipsel_" ]; then
+            echo -e " the cpu is\033[31m $cpu\033[0m ,install mipsel"
+            mipsel_ins
+        else
+            echo "you device can not install the package"
+        fi
+    else
+        echo "file not found"
+        echo "Are you sure it's openwrt?"
+    fi
+    
 }
 
 case $* in
-	mips )
-		mips_ins
-		;;
-	mipsel )
-		mipsel_ins
-		;;
-	arm )
-		arm_ins
-		;;
-	* )
-		defult_ins
-		;;
-
+    mips )
+        mips_ins
+        ;;
+    mipsel )
+        mipsel_ins
+        ;;
+    arm )
+        arm_ins
+        ;;
+    lang )
+        language_ins
+        ;;
+    * )
+        defult_ins
+        ;;
 esac
+language_ins
