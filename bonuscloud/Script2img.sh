8c291aa6de3a1f056613ff2df636546db92b69be
qinghon <wushengshijie@outlook.com>
2019-01-02 15:13:55 +0800

    fixed a bug

--- bonuscloud/Script2img.sh
+++ bonuscloud/Script2img.sh
@@ -6,13 +6,11 @@ MACADDR=`cat /sys/class/net/eth0/address|sed 's/://g'`
 begain(){
     mkdir -p /tmp/opt
     mkdir -p /tmp/etc/network
-
-    cp -r $BCLOUD /tmp/opt
-    cp /etc/network/interfaces /tmp/etc/network/interfaces
     BXC_bcode=$(cat $BCLOUD/bcode)
     BXC_email=$(cat $BCLOUD/email)
     echo -n "{\"bcode\":\"$BXC_bcode\",\"email\":\"$BXC_email\"}" >$BCLOUD/node.db
-
+    cp -r $BCLOUD /tmp/opt
+    cp /etc/network/interfaces /tmp/etc/network/interfaces
     #SET_MAC=`cat /etc/network/interfaces|grep ^hwaddr|grep -o '\(\w\{2\}:\)\{5\}\w\w'`
     #if [ -z "$SET_MAC"  -a "$MACADDR" != "$SET_MAC" ]; then
     #    echo -e "\033[31meth0 mac address Is $MACADDR not equal to set mac $SET_MAC \033[0m"
